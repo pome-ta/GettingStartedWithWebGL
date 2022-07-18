bcb2a04ec7d7f2fecec81175d80cc6e760f374a1
pome-ta <pometarou93@gmail.com>
2022-07-19 00:13:22 +0900

    format

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,5 @@
 import { mat4 } from './gl-matrix.js';
 
-
 //main();
 window.addEventListener('load', main);
 
@@ -11,10 +10,12 @@ function main() {
 
   // WebGL が使用可能で動作している場合にのみ続行します
   if (!gl) {
-    alert('Unable to initialize WebGL. Your browser or machine may not support it.');
+    alert(
+      'Unable to initialize WebGL. Your browser or machine may not support it.'
+    );
     return;
   }
-  
+
   // Vertex shader program
   const vsSource = `
     attribute vec4 aVertexPosition;
@@ -30,7 +31,7 @@ function main() {
       vColor = aVertexColor;
     }
   `;
-  
+
   // Fragment shader program
   const fsSource = `
     varying lowp vec4 vColor;
@@ -42,7 +43,7 @@ function main() {
 
   // シェーダープログラムを初期化します。 これは、頂点などのすべての照明が確立される場所です。
   const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-  
+
   // シェーダープログラムを使用するために必要なすべての情報を収集します。 シェーダープログラムがaVertexPositionに使用している属性を検索し、均一な場所を検索します。
   const programInfo = {
     program: shaderProgram,
@@ -51,17 +52,19 @@ function main() {
       vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
     },
     uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      projectionMatrix: gl.getUniformLocation(
+        shaderProgram,
+        'uProjectionMatrix'
+      ),
       modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
     },
   };
-  
+
   // ここで、描画するすべてのオブジェクトを作成するルーチンを呼び出します。
   const buffers = initBuffers(gl);
-  
+
   drawScene(gl, programInfo, buffers);
 }
-
 
 //
 // initBuffers
@@ -71,24 +74,31 @@ function main() {
 function initBuffers(gl) {
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  
-  const positions = [
-     1.0,  1.0,
-    -1.0,  1.0,
-     1.0, -1.0,
-    -1.0, -1.0,
-  ];
-  
+
+  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+
   // 次に、位置のリストをWebGLに渡して、形状を作成します。 これを行うには、JavaScript配列からFloat32Arrayを作成し、それを使用して現在のバッファーを埋めます。
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
+
   var colors = [
-    1.0,  1.0,  1.0,  1.0,    // white
-    1.0,  0.0,  0.0,  1.0,    // red
-    0.0,  1.0,  0.0,  1.0,    // green
-    0.0,  0.0,  1.0,  1.0,    // blue
+    1.0,
+    1.0,
+    1.0,
+    1.0, // white
+    1.0,
+    0.0,
+    0.0,
+    1.0, // red
+    0.0,
+    1.0,
+    0.0,
+    1.0, // green
+    0.0,
+    0.0,
+    1.0,
+    1.0, // blue
   ];
-  
+
   const colorBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
@@ -99,34 +109,27 @@ function initBuffers(gl) {
   };
 }
 
-
 function drawScene(gl, programInfo, buffers) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
-  
+
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  
-  const fieldOfView = 45 * Math.PI / 180;
+
+  const fieldOfView = (45 * Math.PI) / 180;
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
   const projectionMatrix = mat4.create();
   // 注：glmatrix.jsには、結果を受け取る宛先として常に最初の引数があります。
-  mat4.perspective(projectionMatrix,
-                   fieldOfView,
-                   aspect,
-                   zNear,
-                   zFar);
-  
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+
   const modelViewMatrix = mat4.create();
-  
+
   // 次に、描画位置を、正方形の描画を開始する場所に少し移動します。
-  mat4.translate(modelViewMatrix,
-                 modelViewMatrix,
-                 [-0.0, 0.0, -6.0]);
- 
+  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+
   // 位置バッファからvertexPosition属性に位置を引き出す方法をWebGLに指示します。
   {
     const numComponents = 2;
@@ -136,16 +139,16 @@ function drawScene(gl, programInfo, buffers) {
     const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexPosition);
+      programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
-  
+
   {
     const numComponents = 4;
     const type = gl.FLOAT;
@@ -154,34 +157,34 @@ function drawScene(gl, programInfo, buffers) {
     const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexColor,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexColor);
+      programInfo.attribLocations.vertexColor,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
   }
-  
+
   gl.useProgram(programInfo.program);
   gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
+    programInfo.uniformLocations.projectionMatrix,
+    false,
+    projectionMatrix
+  );
   gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
-  
+    programInfo.uniformLocations.modelViewMatrix,
+    false,
+    modelViewMatrix
+  );
+
   {
     const offset = 0;
     const vertexCount = 4;
     gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
   }
 }
-
-
 
 // シェーダープログラムを初期化して、WebGLがデータの描画方法を認識できるようにします
 function initShaderProgram(gl, vsSource, fsSource) {
@@ -191,13 +194,15 @@ function initShaderProgram(gl, vsSource, fsSource) {
   gl.attachShader(shaderProgram, vertexShader);
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
-  
+
   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    alert(
+      'Unable to initialize the shader program: ' +
+        gl.getProgramInfoLog(shaderProgram)
+    );
     return null;
   }
   return shaderProgram;
-  
 }
 
 // 指定されたタイプのシェーダーを作成し、ソースをアップロードしてコンパイルします。
@@ -205,12 +210,13 @@ function loadShader(gl, type, source) {
   const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  
+
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    alert(
+      'An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader)
+    );
     gl.deleteShader(shader);
     return null;
   }
   return shader;
 }
-
